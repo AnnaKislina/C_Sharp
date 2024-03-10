42d55dc3362ea2c560d0cedf20817dd5c8669fbd
AnnaKislina <kislina.anna@mail.ru>
2024-03-10 18:59:23 +0700

    zadan3

--- sem2/task0/Program.cs
+++ sem2/task0/Program.cs
@@ -5,8 +5,7 @@
 Примеры
 a = 256 => 26
 a = 891 => 81
-
-
+-----------
 Console.WriteLine("Введите трехзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
 
@@ -28,7 +27,7 @@ else {Console.WriteLine("Число не являеться трехзначны
 487 => 8^7 = 2 097 152
 254 => 5^4 = 625
 617 => 1
-*/
+---------
 Console.WriteLine("Введите трехзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
 
@@ -39,3 +38,25 @@ int n2 = num % 10;
 Console.WriteLine(Math.Pow(n1 , n2));
 }
 else {Console.WriteLine("Число не являеться трехзначным");}
+*/
+
+
+/*
+Напишите программу, которая будет принимать на
+вход два числа и выводить, является ли первое число
+кратным второму. Если первое число некратно
+второму, то программа выводит остаток от деления.
+Примеры
+14, 5 => нет, 4
+16, 8 => да
+4, 3 => нет, 1
+*/
+Console.WriteLine("Введите 2 числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % num2 == 0)
+{
+Console.WriteLine("Да");
+}
+else {Console.WriteLine("Нет, "+ num1 % num2);}
