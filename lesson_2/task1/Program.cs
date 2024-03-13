25f569c040f869f6848b28804754132fc3a8128a
AnnaKislina <kislina.anna@mail.ru>
2024-03-13 22:01:09 +0700

    варианты массивов

--- lesson_2/task1/Program.cs
+++ lesson_2/task1/Program.cs
@@ -1,6 +1,4 @@
-﻿
-
-
+﻿/*
 int[] arr = new int[5];
 
 for (int i = 0; i < 5 ; i++)
@@ -8,16 +6,54 @@ for (int i = 0; i < 5 ; i++)
 arr[i] = i;
 Console.WriteLine(arr[i]);
 }
-
+-----------------------------
+*/
 /*
-
 int[] arr = new int[5];
 
 for (int i = 0; i < 5 ; i++)
 {
-arr[i] = Convert.ToInt32(Console.ReadLine;
-Console.WriteLine(arr[i]);
+    arr[i] = Convert.ToInt32(Console.ReadLine);
+    Console.WriteLine(arr[i]);
 }
+---------------------------------------
+*/
+/*
+int n = new Random().Next (1, 10);
+Console.WriteLine(n);
+int[] arr = new int[n];
 
+for (int i = 0; i < n ; i++)
+{
+    arr[i] = i + 1;
+    Console.Write($"{arr[i]} ");
+}
+*/
+/*
+Вывести четные числа массива
+int[] array = {5, 8, 10, 12, 7 , 3, 6, 1, 13, 17};
+for (int i = 0; i < array.Length ; i++)
+{   if (array[i] % 2 == 0)
+    {Console.Write($"{array[i]} ");}
+}
+*/
+/* 
+Задача с гирями по нахождению максимума
+int[] array = {5, 8, 10, 12, 7};
+int max = array [0];
+for (int i = 0; i < array.Length ; i++)
+{   if (array[i] > max)
+    {max = array[i];}
+}
+Console.Write(max);
+*/
+
+/* 
+ЦЫКЛ ДЛЯ МАССИВА ДЛЯ ЧТЕНИЯ!!!! (вывести, посчитать, сумм )
+int[] array = {5, 8, 10, 12, 7};
+foreach (int e in array)
+{
+  Console.Write(e);  
+}
 */
 
