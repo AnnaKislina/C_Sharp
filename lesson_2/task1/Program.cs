246320a496fc36740efe51990a212f341785415c
AnnaKislina <kislina.anna@mail.ru>
2024-03-13 21:09:05 +0700

    добавили обьявления массива

---
+++ lesson_2/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+int[] arr = new int[5];
+
+for (int i = 0; i < 5 ; i++)
+{
+arr[i] = i;
+Console.WriteLine(arr[i]);
+}
+
+/*
+
+int[] arr = new int[5];
+
+for (int i = 0; i < 5 ; i++)
+{
+arr[i] = Convert.ToInt32(Console.ReadLine;
+Console.WriteLine(arr[i]);
+}
+
+*/
+
