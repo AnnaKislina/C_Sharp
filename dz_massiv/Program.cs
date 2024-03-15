2952c93d53fe145cf35d28b66cace2bc174ff577
AnnaKislina <kislina.anna@mail.ru>
2024-03-15 10:38:44 +0700

    домашка по массивам 4 задания

--- dz_massiv/Program.cs
+++ dz_massiv/Program.cs
@@ -41,17 +41,52 @@ System.Console.WriteLine(count);
 Задача 3: Задайте массив из вещественных чисел с
 ненулевой дробной частью. Найдите разницу между
 максимальным и минимальным элементов массива. 
------*/
+-----
 double[] arr = new double[10];
 double max = arr [0];
-double min = arr [0];
+double min = 10;
 
 for (int i = 0; i < arr.Length ; i++)
 {
-    arr[i] = new Random().Next (1, 10);
-    Console.Write($"{arr[i]} ");
+    arr[i] = new Random().NextDouble () *10;
+    Console.WriteLine($"{arr[i]} ");
     if (arr[i] > max) {max = arr[i];}
     if (arr[i] < min) {min = arr[i];}
 }
 System.Console.WriteLine();
-System.Console.WriteLine(max - min);
+System.Console.WriteLine($"max - min = {max - min}");
+*/
+/*-----------------------------------------------------------*/
+/*
+Задача 4**(не обязательно): Дано натуральное
+число в диапазоне от 1 до 100 000. Создайте массив,
+состоящий из цифр этого числа. Старший разряд
+числа должен располагаться на 0-м индексе
+массива, младший – на последнем. Размер массива
+должен быть равен количеству цифр.
+*/
+int n = new Random().Next (1,100001);
+Console.WriteLine(n);
+int dl = n.ToString().Length;
+int[] arr = new int[dl];
+
+/* Первый вариант
+for (int i = 0; i < dl ; i++)
+{
+    arr[dl-i-1] = n % 10;
+    n = n / 10;
+}
+*/
+
+/*Второй вариант*/
+for (int i = 0; i < dl ; i++)
+{
+    arr[i] = n % 10;
+    n = n / 10;
+}
+Array.Reverse(arr);
+
+//Вывод массива
+for (int i = 0; i < dl ; i++)
+{    Console.Write($" {arr[i]} ");}
+
