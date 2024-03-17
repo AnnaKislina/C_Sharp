bd2e0ef5dcb3dcc158b8f7e91a90575f779fb5f3
AnnaKislina <kislina.anna@mail.ru>
2024-03-17 22:43:04 +0700

    задачи лекции 3

---
+++ lesson_3/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Задача 1
+double CalcFormul (int a, int b, int c, int d)
+{
+    double rezult = a * b / (c + d);
+    return rezult;
+}
+
+Console.Write(CalcFormul (1,2,3,4));
+*/
+/*--------------------------------------------*/
+/*Задача 2 Вывод на экран квадратов чисел от1 до n
+
+void PrintSquares (int n)
+{int i=1;
+while (i <= n)
+{
+  Console.Write($"{i*i} ");
+  i++;  
+}
+ Console.WriteLine();}
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
+*/
+/*-------------------------------------------*/
+/*Задача 3 обнулит четные элементы массива
+
+void ZeroCvenMass (int[] arr)
+{
+    for (int i =0; i< arr.Length; i++)
+    {
+        if (arr[i]%2 == 0)
+        {arr[i]=0;}
+    }
+}
+void PrintMass (int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array ={1,2,3,4,5};
+ZeroCvenMass(array);
+PrintMass(array);
+*/
+/*----------------------------------------*/
