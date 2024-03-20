d74f8e9dabf13d01498e25485ab5fa6b6c6a1ac2
AnnaKislina <kislina.anna@mail.ru>
2024-03-20 10:58:53 +0700

    домашка массивы вс плюс автотесты

--- dz_massiv/Program.cs
+++ dz_massiv/Program.cs
@@ -17,6 +17,58 @@ for (int i = 0; i < arr.Length ; i++)
 System.Console.WriteLine();
 System.Console.WriteLine(count);
 */
+/*------------ЗАДАНИЕ ИЗ АВТОТЕСТОВ----------------------------------------*/
+/*
+using System;
+using System.Linq;
+
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{ 
+    public static int CountItemsRange(int[] numbers, int minRange, int maxRange)
+    {
+      int count = 0;
+      for (int i = 0; i < numbers.Length ; i++)
+        { if (numbers[i] >= minRange && numbers[i] <= maxRange)
+                {count = count + 1;}
+         }
+      return count;
+     }
+
+    public static void PrintResult(int[] array)
+    {int count =CountItemsRange(array, 10, 90) ;
+     Console.Write(count);
+    }
+}
+
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        int[] array;
+        if (args.Length >= 1) {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+        } else {
+           // Если аргументов на входе нет
+            array = new int[] {1, 5, 10, 20, 30, 40, 99, 4, 90, 3}; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
+*/
+/*----------------------------------------------------*/
+/*----------------------------------------------------*/
+/*----------------------------------------------------*/
 /*----------------------------------------------------*/
 /*
 Задача 2: Задайте массив на 10 целых чисел.
@@ -36,6 +88,29 @@ for (int i = 0; i < arr.Length ; i++)
 System.Console.WriteLine();
 System.Console.WriteLine(count);
 */
+/*------------из автотестов------------------------------------------*/
+/*
+class UserInputToCompileForTest
+{ 
+    // Подсчет количества четных элементов массива
+    // numbers - массив, в котором ведется подсчет
+    public static int CountEvenItems(int[] numbers)
+    {
+      int count = 0;
+      for (int i = 0; i < numbers.Length ; i++)
+          {if (numbers[i] % 2 == 0){count = count + 1;}}     
+      return count;
+    }
+    
+    public static void PrintResult(int[] array)
+    {int count =CountEvenItems(array) ;
+     Console.Write(count);
+    }
+}
+*/
+/*------------------------------------------------------*/
+/*------------------------------------------------------*/
+/*------------------------------------------------------*/
 /*------------------------------------------------------*/
 /*
 Задача 3: Задайте массив из вещественных чисел с
@@ -56,6 +131,36 @@ for (int i = 0; i < arr.Length ; i++)
 System.Console.WriteLine();
 System.Console.WriteLine($"max - min = {max - min}");
 */
+/*------------из автотестов-----------------------------------------------*/
+/*
+class UserInputToCompileForTest
+{    
+    // Нахождение минимума массива
+    public static double FindMin(double[] numbers)
+    {double min = numbers[0];
+      for (int i = 0; i < numbers.Length ; i++)
+          {if (numbers[i] < min) {min = numbers[i];}}
+     return min;
+    }
+    
+    // Нахождение максимума массива
+    public static double FindMax(double[] numbers)
+    {double max = numbers[0];
+      for (int i = 0; i < numbers.Length ; i++)
+        {if (numbers[i] > max) {max = numbers[i];}}
+     return max;
+    }
+  
+    public static void PrintResult(double[] array)
+    {   double max = FindMax(array);
+        double min = FindMin(array);
+        double rezult = max - min;
+        Console.WriteLine(rezult);
+    }
+}
+*/
+/*-----------------------------------------------------------*/
+/*-----------------------------------------------------------*/
 /*-----------------------------------------------------------*/
 /*
 Задача 4**(не обязательно): Дано натуральное
