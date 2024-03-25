1a6a1f1192a460ca5938d1ca37ebf4f9af479016
AnnaKislina <kislina.anna@mail.ru>
2024-03-25 17:18:31 +0700

    удалить строку и столбец

--- sem5/task1/Program.cs
+++ sem5/task1/Program.cs
@@ -1,2 +1,116 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] Fill2Array (int row, int colum)  //рандом 2-массива
+{
+    Random rnd = new Random();
+    int[,] arr = new int[row,colum];
+
+    for (int i = 0; i < arr.GetLength(0) ; i++)
+    {
+        for (int j = 0; j < arr.GetLength(1) ; j++)
+        {
+            arr[i,j]= rnd.Next(1,11);
+        } 
+    }
+    return arr;
+}
+
+void Print2DArray(int[,] arrayToPrint)  // печать 2-массива
+{
+    Console.Write($"[ ]\t");
+    const int startIndex = 0;
+    for (var i = startIndex + 0; i < startIndex + arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{(i)}]\t");
+    }
+    Console.WriteLine();
+    for (var i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[" + i + "]\t");
+
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+/* 1. Задайте двумерный массив. Найдите элементы, у которых оба
+индекса чётные, и замените эти элементы на их квадраты. */
+int[,] arr = Fill2Array(3,5);
+Print2DArray(arr);
+System.Console.WriteLine();
+/*Print2DArray_New(arr); */
+
+void Print2DArray_New(int[,] arrayToPrint)  // возведение в квдр чет индекс
+{
+    Console.Write($"[ ]\t");
+    const int startIndex = 0;
+    for (var i = startIndex + 0; i < startIndex + arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{(i)}]\t");
+    }
+    Console.WriteLine();
+    for (var i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[" + i + "]\t");
+
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {   
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+            Console.Write(Convert.ToInt32(Math.Pow(arrayToPrint[i, j],2)) + "\t");
+            }
+            else 
+            {
+            Console.Write(arrayToPrint[i, j] + "\t");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+//---------------------------------------------------------------------------
+/* Задание 2. 
+Задайте двумерный массив. Найдите сумму элементов,
+находящихся на главной диагонали (с индексами (0,0); (1;1) и
+т.д. */
+
+int SumDiagonal (int[,] arr)  // сумма диагонали
+{
+    int sum = 0 ;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i == j)
+            {sum = sum + arr[i,j];}   
+        }
+    }
+    return sum;
+}
+
+int sum = SumDiagonal(arr);
+System.Console.WriteLine(sum);
+
+//----------------------------------------------------------------------------------
+/*Задание 3.
+Задайте двумерный массив из целых чисел. Сформируйте новый
+одномерный массив, состоящий из средних арифметических
+значений по строкам двумерного массива. */
+
+double[] SrArifmet(int[,] arr)  //Среднеарифметический по строкам
+{   
+    double[] arr_rezult = new double[arr.GetLength(0)];
+    for (int i = 0; i < arr.GetLength(0) ; i++)
+    {   int sum = 0;
+        for (int j = 0; j < arr.GetLength(1) ; j++)
+        {
+            sum = sum + arr[i,j];
+        } 
+        arr_rezult[i] = sum / arr.GetLength(1); 
+    }
+    return arr_rezult;
+}
+
+
---
+++ sem5/task2/Program.cs
@@ -0,0 +1,111 @@
+﻿/* Задача 4*(не обязательная): Задайте двумерный массив
+из целых чисел. Напишите программу, которая удалит
+строку и столбец, на пересечении которых расположен
+наименьший элемент массива. Под удалением
+понимается создание нового двумерного массива без
+строки и столбца */
+Console.Clear();
+
+
+int[,] Get2DArray(int row, int column) //рандом 2-массива
+{
+    int[,] array = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)  //печать 2-массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+(int, int) FiendMinIndex (int[,] array)  //индексы минимального значения
+{
+    int minRow = 0;
+    int minCol = 0;
+    int min = array[0,0];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+          if (array[i,j] < min)
+          {
+            minRow = i;
+            minCol = j;
+            min = array[i,j];
+          }   
+        }
+    }
+    return (minRow, minCol);
+}
+
+/* int[,] DelColRow (int[,] array, int row, int col);
+{
+    int[,] arrRezult = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];
+    int x = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {   
+        if (i == row)
+        {
+            row = -1;
+            continue;
+        }
+        int y = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == col)
+            {
+                continue;
+            }
+            arrRezult[x,y]= array[i,j];
+            y++;
+        }
+        x++;
+    }
+    return arrRezult;
+}*/
+
+int[,] DeleteMinColumnRow(int[,] array, int row, int column)
+{
+    int[,] result = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];
+    int x = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (i == row)
+        {
+            continue;
+        }
+        int y = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == column) continue;
+            result[x, y] = array[i, j];
+            y++;
+        }
+        x++;
+    }
+    return result;
+} 
+
+
+int[,] userArray = Get2DArray(4,4);
+Print2DArray(userArray);
+(int minRow, int minCol) = FiendMinIndex(userArray);
+System.Console.WriteLine($"{minRow}  {minCol}");
+int[,] newArray = DeleteMinColumnRow(userArray,minRow,minCol);
+System.Console.WriteLine();
+Print2DArray(newArray);
