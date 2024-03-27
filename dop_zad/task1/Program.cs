9487011d9368dfcec8cecc65eaa8df1d1b784287
AnnaKislina <kislina.anna@mail.ru>
2024-03-27 18:26:41 +0700

    125

---
+++ dop_zad/task1/Program.cs
@@ -0,0 +1,68 @@
+﻿Console.Clear();
+
+int[,] Fill2Array (int row, int colum) //рандом 2-массива
+{
+Random rnd = new Random();
+int[,] arr = new int[row,colum];
+
+for (int i = 0; i < arr.GetLength(0) ; i++)
+{
+   for (int j = 0; j < arr.GetLength(1) ; j++)
+   {
+    arr[i,j]= rnd.Next(1,11);
+   } 
+}
+return arr;
+}
+
+
+void Print2DArray(int[,] array)  //печать 2-массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+/* Задача 3. В заданной строке текста определите 
+количество слов. Каждое слово отделено друг от друга пробелом.*/
+
+/* System.Console.WriteLine("Введите что нибудь");
+string st = Console.ReadLine();
+int count = 0;
+
+for (int i = 0; i < st.Length; i++)
+{
+    if (st[i] == ' ') count++;
+}
+System.Console.WriteLine(count);
+ */
+//--------------------------------------------------------------
+/* В массиве строк (список фамилий) определите самую длинную фамилию. */
+
+string[] array = ["Петров","Иванов","Сидоров","Путин","Копиленов"];
+
+
+/* for(int i = 0; i < stringArray.Length; i++)
+{
+   stringArray[i] = Console.ReadLine();
+
+} */
+
+int maxInd = 0;
+int maxDl = array[0].Length;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length > maxDl)
+    {
+        maxInd = i;
+        maxDl = array[i].Length;
+    }
+}
+System.Console.WriteLine(array[maxInd]); 
